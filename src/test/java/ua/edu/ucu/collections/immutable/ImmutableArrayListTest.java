74f688de44f5e5de61820df561309f979b4a8a45
MeriDm7 <dmytryshyn.m@ucu.edu.ua>
2019-11-04 23:35:23 +0200

    .

--- src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
+++ src/test/java/ua/edu/ucu/collections/immutable/ImmutableArrayListTest.java
@@ -4,20 +4,24 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ImmutableArrayListTest {
+
+    ImmutableArrayList empty = new ImmutableArrayList();
+    ImmutableArrayList lst = new ImmutableArrayList(new Object[] {1, 2, 3});
     
     @Test
     public void testAddMethod1() {
-        Object[] simpleArray = new Object[]{1, 2, 3};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
         ImmutableArrayList res =  (ImmutableArrayList) lst.add(5);
         Object[] resArray = new Object[]{1, 2, 3, 5};
         assertArrayEquals(resArray, res.toArray());
     }
 
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddEmptyMethod2() {
+        ImmutableArrayList res =  (ImmutableArrayList) empty.add(5, 1);
+    }
+
     @Test
     public void testAddMethod2() {
-        Object[] simpleArray = new Object[]{1, 2, 3};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
         ImmutableArrayList res =  (ImmutableArrayList) lst.add(2, 7);
         Object[] resArray = new Object[]{1, 2, 7, 3};
         assertArrayEquals(resArray, res.toArray());
@@ -25,61 +29,89 @@ public class ImmutableArrayListTest {
 
     @Test
     public void testAddAllMethod1() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
         Object[] addArray = new Object[]{5, 6, 7};
         ImmutableArrayList res =  (ImmutableArrayList) lst.addAll(addArray);
-        Object[] resArray = new Object[]{0, 1, 2, 3, 5, 6, 7};
+        Object[] resArray = new Object[]{1, 2, 3, 5, 6, 7};
         assertArrayEquals(resArray, res.toArray());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAddAllEmptyMethod2() {
+        Object[] addArray = new Object[]{6, 7, 8, 9, 10};
+        ImmutableArrayList res =  (ImmutableArrayList) empty.addAll(5, addArray);
     }
 
     @Test
     public void testAddAllMethod2() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3, 4, 5, 11};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
         Object[] addArray = new Object[]{6, 7, 8, 9, 10};
-        ImmutableArrayList res =  (ImmutableArrayList) lst.addAll(3, addArray);
-        Object[] resArray = new Object[]{0, 1, 2, 6, 7, 8, 9, 10, 3, 4, 5, 11};
+        ImmutableArrayList res =  (ImmutableArrayList) lst.addAll(2, addArray);
+        Object[] resArray = new Object[]{1, 2, 6, 7, 8, 9, 10, 3};
         assertArrayEquals(resArray, res.toArray());
     }
 
     @Test
     public void testGetMethod() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3, 4, 5, 11};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
-        int indx = 6;
+        int indx = 2;
         Object res =   lst.get(indx);
-        Object exp = 11;
+        Object exp = 3;
         assertEquals(exp, res);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testGetEmptyMethod() {
+        int indx = 2;
+        Object res =  empty.get(indx);
     }
 
     @Test
     public void testRemoteMethod() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3, 4, 5, 11};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
-        int el = 5;
+        int el = 1;
         ImmutableArrayList res =  (ImmutableArrayList) lst.remove(el);
-        Object[] resArray = new Object[]{0, 1, 2, 3, 4, 11};
+        Object[] resArray = new Object[]{1, 3};
         assertArrayEquals(resArray, res.toArray());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testRemoteEmptyMethod() {
+        int el = 4;
+        ImmutableArrayList res =  (ImmutableArrayList) lst.remove(el);
     }
 
     @Test
     public void testSetMethod() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3, 4, 5, 11};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
         int el = 5;
         ImmutableArrayList res =  (ImmutableArrayList) lst.set(0, el);
-        Object[] resArray = new Object[]{5, 1, 2, 3, 4, 5, 11};
+        Object[] resArray = new Object[]{5, 2, 3};
         assertArrayEquals(resArray, res.toArray());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetEmptyMethod() {
+        int el = 4;
+        int indx = 10;
+        ImmutableArrayList res =  (ImmutableArrayList) lst.set(indx, el);
     }
 
     @Test
     public void testIndexOfMethod() {
-        Object[] simpleArray = new Object[]{0, 1, 2, 3, 4, 5, 11};
-        ImmutableArrayList lst = new ImmutableArrayList(simpleArray);
+        int el = 2;
+        int res =  lst.indexOf(el);
+        int exp = 1;
+        assertEquals(exp, res);
+    }
+
+    @Test
+    public void testIndexOfNoneMethod() {
         int el = 12;
         int res =  lst.indexOf(el);
         int exp = -1;
+        assertEquals(exp, res);
+    }
+
+    @Test
+    public void testSizeMethod() {
+        int res =  lst.size();
+        int exp = 3;
         assertEquals(exp, res);
     }
     
