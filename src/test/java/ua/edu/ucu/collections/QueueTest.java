ae4b32ff1a978a07009dba0753ae318630c81458
MeriDm7 <dmytryshyn.m@ucu.edu.ua>
2019-11-05 00:57:42 +0200

    .

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,38 @@
 package ua.edu.ucu.collections;
 
 import org.junit.Test;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 import static org.junit.Assert.*;
 
 public class QueueTest {
+
+    private Queue q = new Queue(new ImmutableLinkedList(new Object[] {3, 5, 7}));
+    private Queue empty = new Queue();
     
     @Test
-    public void testSomeMethod() {
+    public void testPeek() {
+        assertEquals(3, q.peek());
     }
-    
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testPeekException() {
+        empty.peek();
+    }
+
+    @Test
+    public void testDeQueue() {
+        assertEquals(3, q.dequeue());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testDeQueueException() {
+        empty.dequeue();
+    }
+
+    @Test
+    public void testEnQueue() {
+        empty.enqueue(7);
+        assertEquals(7, empty.peek());
+    }
 }
